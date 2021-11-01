c7235077bd74e610d0620f13c5b55b56ac43b120
ghaziankamil <16520465@std.stei.itb.ac.id>
2021-11-01 22:51:05 +0700

    tambahin ADT progress list

--- src/lib/game/progress_list/progress_list.h
+++ src/lib/game/progress_list/progress_list.h
@@ -5,15 +5,25 @@
 
 typedef LinkedList ProgressList;
  
-typedef struct daftar {
+typedef Order PlElType;
 
-    Order order;
+/*Membuat progress list yang baru*/
+void pl_create_progress(ProgressList *pl);
 
-}Daftar;
+/*Menghasilkan true apabila progress list masih kosong*/
+boolean pl_is_empty(ProgressList pl);
 
+/*Menambahkan order baru kedalam progress list*/
+/*order baru tersebut menjadi elemen terakhir dari progress list*/
+void pl_insert_last(ProgressList *pl, PlElType order);
 
-void p_create_progress(ProgressList *pl);
-void p_display_progress(ProgressList pl);
+/*Menghapus order pertama dari progress list */
+/*Hal ini terjadi ketika order telah selesai diantarkan*/
+void pl_delete_first(ProgressList *pl, PlElType *order);
+
+/*Menampilkan isi progress list ke layar*/
+void pl_display_progress(ProgressList pl);
+
 
 
 
