4516d442b1413a76e3703fe46c97ab1dc9e5563c
malifpy <malifputrayasa@gmail.com>
2021-11-07 17:50:20 +0700

    Implementasi Inventory Gadget
    Implementasi dari header inventory_gadget.h
    
    ADT Gadget agak sus, karena dia malah tipe char, bukan char[] atau
    struct. Inventory Gadget akan disesuaikan apabila ada perubahan terhadap
    ADT Gadget.

--- .ccls-cache/@home@malifpy@Alif@Git@TubesAlstrukdat/src@lib@game@game.h
+++
@@ -1,9 +0,0 @@
-#ifndef H_GAME
-#define H_GAME
-
-
-#include "../structure/structure.h"
-#include "td_list/td_list.h"
-#include "progress_list/progress_list.h"
-
-#endif
--- .ccls-cache/@home@malifpy@Alif@Git@TubesAlstrukdat/src@lib@prototype@prototype.h
+++
@@ -1,8 +0,0 @@
-#ifndef H_PROTOTYPE_MOBILITA
-#define H_PROTOTYPE_MOBILITA
-
-#include "../boolean.h"
-#include "gadget/gadget.h"
-#include "order/order.h"
-
-#endif
