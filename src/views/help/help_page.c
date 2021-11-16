f29bbe470d87d1b3ba414941902fa2974eaaf810
jakartasipirok <kevinroni2002@gmail.com>
2021-11-16 23:56:42 +0700

    garap help page

---
+++ src/views/help/help_page.c
@@ -0,0 +1,12 @@
+#include "help_page.h"
+void show_help(){
+    printf("1. MOVE -> Berpindah ke sebuah lokasi");
+    printf("2. PICK_UP -> Mengambil item di lokasi sekarang");
+    printf("3. DROP_OFF -> Mengantarkan item ke lokasi sekarang");
+    printf("4. MAP -> Melihat peta");
+    printf("5. TO_DO -> Melihat semua pesanan");
+    printf("6. IN_PROGRESS -> Melihat pesanan yang berlansgung");
+    printf("7. BUY -> Membeli gadget ");
+    printf("8. INVENTORY -> Melihat isi inventory");
+    printf("9. HELP -> Melihat laman bantuan");
+}
