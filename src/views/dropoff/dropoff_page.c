e60b1392067967f4dabc3034e6fb1a86ba6d631f
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-11-17 18:09:32 +0700

    Memperbaiki Dropoff yang salah isi

--- src/views/dropoff/dropoff_page.c
+++ src/views/dropoff/dropoff_page.c
@@ -1,54 +1,53 @@
-// #include "dropoff_page.h"
+#include "dropoff_page.h"
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// boolean dp_is_any_vip() { return pl_is_any_vip(s_status_game.progress_list);
-// }
+boolean dp_is_any_vip() { return pl_is_any_vip(s_status_game.progress_list); }
 
-// boolean dp_is_dropable() { return true; }
+boolean dp_is_dropable() { return true; }
 
-// int dp_get_prize(Item i) {
-//   switch (i.type) {
-//     case 'N':
-//       return 200;
-//     case 'H':
-//     case 'P':
-//       return 400;
-//     case 'V':
-//       return 600;
-//   }
-// }
+int dp_get_prize(Item i) {
+  switch (i.type) {
+    case 'N':
+      return 200;
+    case 'H':
+    case 'P':
+      return 400;
+    case 'V':
+      return 600;
+  }
+}
 
-// void show_dropoff() {
-//   if (!t_is_empty(s_status_game.tas_mobita)) {
-//     if (!dp_is_any_vip()) {
-//       Item obj = S_TOP(t_cap(s_status_game.tas_mobita));
-//       Order* top = pl_get_order_by_item(s_status_game.progress_list, obj);
+void show_dropoff() {
+  if (!t_is_empty(s_status_game.tas_mobita)) {
+    if (!dp_is_any_vip()) {
+      Item obj = S_TOP(t_isi(s_status_game.tas_mobita));
+      Order* top = pl_get_order_by_item(s_status_game.progress_list, obj);
 
-//       if (top && b_is_equal(top->dropOff, s_status_game.posisi_sekarang)) {
-//         int harga = dp_get_prize(obj);
+      if (top && b_is_equal(top->dropOff, s_status_game.posisi_sekarang)) {
+        int harga = dp_get_prize(obj);
 
-//         char pesan[200];
-//         snprintf(pesan, 200,
-//                  "Yaay 🎉, kamu berhasil mengantarkan barang %s.\nKamu "
-//                  "mendapatkan %d Yen.",
-//                  i_item_type_name(obj), harga);
+        char pesan[200];
+        snprintf(pesan, 200,
+                 "Yaay 🎉, kamu berhasil mengantarkan barang %s.\nKamu "
+                 "mendapatkan %d Yen.",
+                 i_item_type_name(obj), harga);
 
-//         cm_modal_info(pesan);
-//         pl_delete_by_order(&(s_status_game.progress_list), *top);
-//         t_take_item(&(s_status_game.tas_mobita));
-//         s_status_game.uang_mobita += harga;
-//       } else {
-//         cm_modal_info("Tidak ada yang bisa di drop off disini.");
-//       }
-//     } else {
-//       printf("\n");
-//       cm_modal_warning(
-//           "Anda sedang mengambil pesanan VIP.\nSilahkan antarkan terlebih "
-//           "dahulu.");
-//     }
-//   } else {
-//     printf("\n");
-//     cm_modal_info("Tas kamu saat ini kosong.\nSilahkan ambil pesanan dulu.");
-//   }
-// }
+        cm_modal_info(pesan);
+        pl_delete_by_order(&(s_status_game.progress_list), *top);
+        t_take_item(&(s_status_game.tas_mobita));
+        s_status_game.uang_mobita += harga;
+      } else {
+        cm_modal_info("Tidak ada yang bisa di drop off disini.");
+      }
+    } else {
+      printf("\n");
+      cm_modal_warning(
+          "Anda sedang mengambil pesanan VIP.\nSilahkan antarkan terlebih "
+          "dahulu.");
+    }
+  } else {
+    printf("\n");
+    cm_modal_info("Tas kamu saat ini kosong.\nSilahkan ambil pesanan dulu.");
+  }
+}
